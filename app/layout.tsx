f39959b97cfe5cf5f95652510375853461d8d3cb
Stefan Strobitzki <s.strobitzki@icompetence.de>
2025-12-09 17:27:20 +0100

    UC & GTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,53 @@
-import type { Metadata } from 'next'
-import '../src/index.css'
+import type { Metadata } from 'next';
+import '../src/index.css';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: 'iCompetence - We enable companies for the Agentic Era',
   description: 'We enable companies for the Agentic Era on a trustworthy data foundation.',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
+  const GTM_ID = 'GTM-WMTT46J';
+  const USERCENTRICS_ID = 'gRxSmB1lD';
+
   return (
     <html lang="de">
-      <body>{children}</body>
+      <head>
+        <Script
+          id="usercentrics-cmp"
+          src="https://web.cmp.usercentrics.eu/ui/loader.js"
+          data-settings-id={USERCENTRICS_ID}
+          strategy="beforeInteractive"
+        />
+        <Script
+          id="gtm-data-layer"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
+      </head>
+
+      <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
